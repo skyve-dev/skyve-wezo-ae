fe10ebc10156544e72abca46ef0a6e9b6a6918fd
Arif Rachim <a.arif.r@gmail.com>
2025-08-11 23:35:02 +0400

    Switch to PostgreSQL database and update Prisma schema. Add user roles, update dependencies, and adjust environment configurations.

---
+++ server/prisma/migrations/20250811192722_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "public"."UserRole" AS ENUM ('TENANT', 'HOMEOWNER', 'MANAGER');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."UserRole" NOT NULL DEFAULT 'TENANT',
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "passwordResetToken" TEXT,
+    "passwordResetExpires" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_passwordResetToken_key" ON "public"."User"("passwordResetToken");
